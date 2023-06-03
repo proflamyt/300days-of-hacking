806bea22cdd756ec3750d840ed161a11930a913c
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-06-03 18:58:00 +0100

    added process creation

--- Topic41/README.md
+++ Topic41/README.md
@@ -12,3 +12,8 @@ DLL:
 gcc .\randomDll.cpp --shared -o outputfile.dll
 rundll32.exe .\outputfile.dll,DllMain
 ```
+
+
+
+
+https://www.elastic.co/blog/ten-process-injection-techniques-technical-survey-common-and-trending-process
--- Topic41/profdll.cpp
+++ Topic41/profdll.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
     }
 
 
-     rBuffer = VirtualAllocEx(hProcess, NULL, DLLsize, 
+    rBuffer = VirtualAllocEx(hProcess, NULL, DLLsize, 
                             (MEM_COMMIT|MEM_RESERVE), PAGE_READWRITE);
 
 
